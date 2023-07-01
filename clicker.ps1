4eb53d0b1c9190c581b9a58af4d0a9436344d129
xfy <135300152+xfy777@users.noreply.github.com>
2023-07-01 02:26:00 -0400

    adding keyboard entry detection to pause clicking

--- clicker.ps1
+++ clicker.ps1
@@ -2,25 +2,64 @@ Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
 
-public class User32 {
-    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-}
+public class CustomMouseClicker {
+    [DllImport("user32.dll")]
+    public static extern bool GetCursorPos(out POINT lpPoint);
 
-public class Clicker {
-    public static void Main(string[] args) {
-        while (true) {
-            $cursorPosition = [System.Windows.Forms.Cursor]::Position
-            $x = [System.Math]::Min([System.Math]::Max($cursorPosition.X, 0), [System.Windows.Forms.SystemInformation]::VirtualScreen.Width)
-            $y = [System.Math]::Min([System.Math]::Max($cursorPosition.Y, 0), [System.Windows.Forms.SystemInformation]::VirtualScreen.Height)
+    [DllImport("user32.dll")]
+    public static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
 
-            [User32]::mouse_event(0x02, $x, $y, 0, 0); 
-            [User32]::mouse_event(0x04, $x, $y, 0, 0);  
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT {
+        public int X;
+        public int Y;
+    }
 
-            Start-Sleep -Seconds 10
-        }
+    public static void ClickLeftMouseButton(int x, int y) {
+        mouse_event(0x0002, x, y, 0, UIntPtr.Zero); 
+        mouse_event(0x0004, x, y, 0, UIntPtr.Zero); 
+        mouse_event(0x0004, x, y, 0, UIntPtr.Zero); 
     }
 }
 "@
 
-[Clicker]::Main()
+$previousCursorPosition = New-Object CustomMouseClicker+POINT
+$previousCursorPosition.X = -1
+$previousCursorPosition.Y = -1
+
+while ($true) {
+    $cursorPosition = New-Object CustomMouseClicker+POINT
+    [CustomMouseClicker]::GetCursorPos([ref]$cursorPosition)
+
+    $keyboardInputDetected = $false
+    $mouseMovementDetected = $false
+
+    $clickStartTime = Get-Date
+    while ((Get-Date) -lt $clickStartTime.AddSeconds(10)) {
+        $keyState = [System.Console]::KeyAvailable
+        if ($keyState) {
+            $keyboardInputDetected = $true
+            Write-Host "Keyboard input detected."
+            break
+        }
+        Start-Sleep -Milliseconds 100
+
+        $currentCursorPosition = New-Object CustomMouseClicker+POINT
+        [CustomMouseClicker]::GetCursorPos([ref]$currentCursorPosition)
+        if ($currentCursorPosition.X -ne $cursorPosition.X -or $currentCursorPosition.Y -ne $cursorPosition.Y) {
+            $mouseMovementDetected = $true
+            Write-Host "Mouse movement detected."
+            break
+        }
+    }
+
+    if (-not $keyboardInputDetected -and -not $mouseMovementDetected) {
+        [CustomMouseClicker]::ClickLeftMouseButton($cursorPosition.X, $cursorPosition.Y)
+    }
+    else {
+        Write-Host "Clicking stopped due to keyboard input or mouse movement."
+    }
+
+    Start-Sleep -Seconds 10
+}
+
